7aa400af0c57f6bc0ac5763feea1871d3a211e5e
Athaws <rasmusm96@yahoo.se>
2023-12-04 20:04:27 +0100

    day2 comments

--- day02/day2part1.py
+++ day02/day2part1.py
@@ -4,24 +4,23 @@ sum = 0
 with open('input.txt', 'r') as f:
     lines = f.readlines()
     for line in lines:
-        line = line.replace('\n','').replace('Game ','').replace(',','')
-        line = line.split(':')
+        line = line.replace('\n','').replace('Game ','').replace(',','')    # Remove fluff from line
+        line = line.split(':')                                              # Split gamenumber from rest
         impossible, gamenumber = False, int(line[0])
-        for parts in line[1].split(';'):
-            if impossible:
+        for parts in line[1].split(';'):                                    # Split line by sub-games
+            if impossible:                      # If game is impossible, immediately move to next game
                 break
-            parts = parts.split(' ')
+            parts = parts.split(' ')            # Split subgame into respective number and colour
             for i in range(1, len(parts), 2):
-                print(parts[i], parts[i+1])
-                if int(parts[i]) > MAXRED and parts[i+1] == 'red':
+                if int(parts[i]) > MAXRED and parts[i+1] == 'red':          # Check first number and colour
                     impossible = True
                     break
-                elif int(parts[i]) > MAXGREEN and parts[i+1] == 'green':
+                elif int(parts[i]) > MAXGREEN and parts[i+1] == 'green':    # Check second number and colour
                     impossible = True
                     break
-                elif int(parts[i]) > MAXBLUE and parts[i+1] == 'blue':
+                elif int(parts[i]) > MAXBLUE and parts[i+1] == 'blue':      # Check third number and colour
                     impossible = True
                     break
-        if not impossible:
+        if not impossible:          # If none of the checks flagged for impossibility, add game-number to sum
             sum += gamenumber
 print(sum)
--- day02/day2part2.py
+++ day02/day2part2.py
@@ -2,18 +2,18 @@ sum = 0
 with open('input.txt', 'r') as f:
     lines = f.readlines()
     for line in lines:
-        line = line.replace('\n','').replace('Game ','').replace(',','')
-        line = line.split(':')
+        line = line.replace('\n','').replace('Game ','').replace(',','')    # Remove fluff from string
+        line = line.split(':')                                              # Split gamenumber from rest
         impossible, gamenumber = False, int(line[0])
         minRed, minGreen, minBlue = 1, 1, 1
-        for parts in line[1].split(';'):
-            parts = parts.split(' ')
+        for parts in line[1].split(';'):                                    # Split line by sub-games
+            parts = parts.split(' ')                        # Split subgame into respective number and colour
             for i in range(1, len(parts), 2):
-                if int(parts[i]) > minRed and parts[i+1] == 'red':
-                    minRed = int(parts[i])
+                if int(parts[i]) > minRed and parts[i+1] == 'red':  
+                    minRed = int(parts[i])      # Min. reds is updated if higher found in current subgame
                 elif int(parts[i]) > minGreen and parts[i+1] == 'green':
-                    minGreen = int(parts[i])
+                    minGreen = int(parts[i])    # Min. greens is updated if higher found in current subgame
                 elif int(parts[i]) > minBlue and parts[i+1] == 'blue':
-                    minBlue = int(parts[i])
-        sum += minRed * minGreen * minBlue
+                    minBlue = int(parts[i])     # Min. blues is updated if higher found in current subgame
+        sum += minRed * minGreen * minBlue      # Sum of the minumum of all colours added to sum
 print(sum)
